7facdd1e39f705110fa67468dd39706ef859f1ce
Scott Lowe <scott.code.lowe@gmail.com>
2017-08-28 22:52:35 +0100

    BUG: Download inception checkpoint for l2 sub

--- defenses/l2_sub/download_resources.sh
+++ defenses/l2_sub/download_resources.sh
@@ -6,6 +6,15 @@
 # directory where this script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+cd "${SCRIPT_DIR}"
+
+if [ ! -e "inception_v3.ckpt" ]; then
+    # Control will enter here if file doesn't exist.
+    wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+    tar -xvzf inception_v3_2016_08_28.tar.gz
+    rm inception_v3_2016_08_28.tar.gz
+fi
+
 if [ ! -d "${SCRIPT_DIR}/comparison_images" ]; then
     # Control will enter here if $DIRECTORY doesn't exist.
     mkdir "${SCRIPT_DIR}/comparison_images"
