c2bb17c111ee984a44f29f501ad2f99774c9fe83
Scott Lowe <scott.code.lowe@gmail.com>
2017-08-29 23:30:58 +0100

    Add targeted attack ensembling two models

---
+++ targeted_attacks/t_iter_ens_I3_EAIR2/download_resources.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Script to download checkpoints and other resources for the model.
+#
+
+# directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}"
+
+if [ ! -e "inception_v3.ckpt" ]; then
+    # Control will enter here if file doesn't exist.
+    wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+    tar -xvzf inception_v3_2016_08_28.tar.gz
+    rm inception_v3_2016_08_28.tar.gz
+fi
+
+if [ ! -e "ens_adv_inception_resnet_v2.ckpt.data-00000-of-00001" ]; then
+    # Download ensemble adversarially trained inception resnet v2 checkpoint
+    wget http://download.tensorflow.org/models/ens_adv_inception_resnet_v2_2017_08_18.tar.gz
+    tar -xvzf ens_adv_inception_resnet_v2_2017_08_18.tar.gz
+    rm ens_adv_inception_resnet_v2_2017_08_18.tar.gz
+fi
+
