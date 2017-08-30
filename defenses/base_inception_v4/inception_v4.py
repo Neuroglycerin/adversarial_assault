141c674c1263a049847675aa801ef0fb23e89365
Scott Lowe <scott.code.lowe@gmail.com>
2017-08-30 18:56:31 +0100

    BUG: Incorporate inception arg scope for incepv4

--- defenses/base_inception_v4/inception_v4.py
+++ defenses/base_inception_v4/inception_v4.py
@@ -26,8 +26,6 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-from tensorflow.contrib.slim.nets import inception_utils
-
 slim = tf.contrib.slim
 
 
@@ -320,4 +318,45 @@ def inception_v4(inputs, num_classes=1001, is_training=True,
 inception_v4.default_image_size = 299
 
 
-inception_v4_arg_scope = inception_utils.inception_arg_scope
+def inception_arg_scope(weight_decay=0.00004,
+                        use_batch_norm=True,
+                        batch_norm_decay=0.9997,
+                        batch_norm_epsilon=0.001):
+  """Defines the default arg scope for inception models.
+
+  Args:
+    weight_decay: The weight decay to use for regularizing the model.
+    use_batch_norm: "If `True`, batch_norm is applied after each convolution.
+    batch_norm_decay: Decay for batch norm moving average.
+    batch_norm_epsilon: Small float added to variance to avoid dividing by zero
+      in batch norm.
+
+  Returns:
+    An `arg_scope` to use for the inception models.
+  """
+  batch_norm_params = {
+      # Decay for the moving averages.
+      'decay': batch_norm_decay,
+      # epsilon to prevent 0s in variance.
+      'epsilon': batch_norm_epsilon,
+      # collection containing update_ops.
+      'updates_collections': tf.GraphKeys.UPDATE_OPS,
+  }
+  if use_batch_norm:
+    normalizer_fn = slim.batch_norm
+    normalizer_params = batch_norm_params
+  else:
+    normalizer_fn = None
+    normalizer_params = {}
+  # Set weight_decay for weights in Conv and FC layers.
+  with slim.arg_scope([slim.conv2d, slim.fully_connected],
+                      weights_regularizer=slim.l2_regularizer(weight_decay)):
+    with slim.arg_scope(
+        [slim.conv2d],
+        weights_initializer=slim.variance_scaling_initializer(),
+        activation_fn=tf.nn.relu,
+        normalizer_fn=normalizer_fn,
+        normalizer_params=normalizer_params) as sc:
+      return sc
+
+inception_v4_arg_scope = inception_arg_scope
