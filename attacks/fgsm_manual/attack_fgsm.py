651895c5ebc293d864a9c2d5b1076fb5d03273ac
Scott Lowe <scott.code.lowe@gmail.com>
2017-09-01 20:05:31 +0100

    BUG: Fix manual fgsm

--- attacks/fgsm_manual/attack_fgsm.py
+++ attacks/fgsm_manual/attack_fgsm.py
@@ -43,95 +43,107 @@ FLAGS = tf.flags.FLAGS
 
 
 def load_images(input_dir, batch_shape):
-  """Read png images from input directory in batches.
+    """Read png images from input directory in batches.
 
-  Args:
-    input_dir: input directory
-    batch_shape: shape of minibatch array, i.e. [batch_size, height, width, 3]
+    Args:
+      input_dir: input directory
+      batch_shape: shape of minibatch array, i.e. [batch_size, height, width, 3]
 
-  Yields:
-    filenames: list file names without path of each image
-      Lenght of this list could be less than batch_size, in this case only
-      first few images of the result are elements of the minibatch.
-    images: array with all images from this batch
-  """
-  images = np.zeros(batch_shape)
-  filenames = []
-  idx = 0
-  batch_size = batch_shape[0]
-  for filepath in tf.gfile.Glob(os.path.join(input_dir, '*.png')):
-    with tf.gfile.Open(filepath) as f:
-      image = np.array(Image.open(f).convert('RGB')).astype(np.float) / 255.0
-    # Images for inception classifier are normalized to be in [-1, 1] interval.
-    images[idx, :, :, :] = image * 2.0 - 1.0
-    filenames.append(os.path.basename(filepath))
-    idx += 1
-    if idx == batch_size:
-      yield filenames, images
-      filenames = []
-      images = np.zeros(batch_shape)
-      idx = 0
-  if idx > 0:
-    yield filenames, images
+    Yields:
+      filenames: list file names without path of each image
+        Lenght of this list could be less than batch_size, in this case only
+        first few images of the result are elements of the minibatch.
+      images: array with all images from this batch
+    """
+    images = np.zeros(batch_shape)
+    filenames = []
+    idx = 0
+    batch_size = batch_shape[0]
+    for filepath in tf.gfile.Glob(os.path.join(input_dir, '*.png')):
+        with tf.gfile.Open(filepath) as f:
+            image = np.array(
+                Image.open(
+                    f).convert(
+                        'RGB')).astype(
+                np.float) / 255.0
+        # Images for inception classifier are normalized to be in [-1, 1]
+        # interval.
+        images[idx, :, :, :] = image * 2.0 - 1.0
+        filenames.append(os.path.basename(filepath))
+        idx += 1
+        if idx == batch_size:
+            yield filenames, images
+            filenames = []
+            images = np.zeros(batch_shape)
+            idx = 0
+    if idx > 0:
+        yield filenames, images
 
 
 def save_images(images, filenames, output_dir):
-  """Saves images to the output directory.
+    """Saves images to the output directory.
 
-  Args:
-    images: array with minibatch of images
-    filenames: list of filenames without path
-      If number of file names in this list less than number of images in
-      the minibatch then only first len(filenames) images will be saved.
-    output_dir: directory where to save images
-  """
-  for i, filename in enumerate(filenames):
-    # Images for inception classifier are normalized to be in [-1, 1] interval,
-    # so rescale them back to [0, 1].
-    with tf.gfile.Open(os.path.join(output_dir, filename), 'w') as f:
-      img = (((images[i, :, :, :] + 1.0) * 0.5) * 255.0).astype(np.uint8)
-      Image.fromarray(img).save(f, format='PNG')
+    Args:
+      images: array with minibatch of images
+      filenames: list of filenames without path
+        If number of file names in this list less than number of images in
+        the minibatch then only first len(filenames) images will be saved.
+      output_dir: directory where to save images
+    """
+    for i, filename in enumerate(filenames):
+        # Images for inception classifier are normalized to be in [-1, 1] interval,
+        # so rescale them back to [0, 1].
+        with tf.gfile.Open(os.path.join(output_dir, filename), 'w') as f:
+            img = (((images[i, :, :, :] + 1.0) * 0.5) * 255.0).astype(np.uint8)
+            Image.fromarray(img).save(f, format='PNG')
 
 
 def main(_):
-  # Images for inception classifier are normalized to be in [-1, 1] interval,
-  # eps is a difference between pixels so it should be in [0, 2] interval.
-  # Renormalizing epsilon from [0, 255] to [0, 2].
-  eps = 2.0 * FLAGS.max_epsilon / 255.0
-  batch_shape = [FLAGS.batch_size, FLAGS.image_height, FLAGS.image_width, 3]
-  num_classes = 1001
+    # Images for inception classifier are normalized to be in [-1, 1] interval,
+    # eps is a difference between pixels so it should be in [0, 2] interval.
+    # Renormalizing epsilon from [0, 255] to [0, 2].
+    eps = 2.0 * FLAGS.max_epsilon / 255.0
+    batch_shape = [FLAGS.batch_size, FLAGS.image_height, FLAGS.image_width, 3]
+    num_classes = 1001
 
-  tf.logging.set_verbosity(tf.logging.INFO)
+    tf.logging.set_verbosity(tf.logging.INFO)
 
-    with slim.arg_scope(inception.inception_v3_arg_scope()):
-      inception.inception_v3(
-          x_input, num_classes=num_classes, is_training=False)
+    with tf.Graph().as_default():
+        # Prepare graph
+        x_input = tf.placeholder(tf.float32, shape=batch_shape)
+        x_max = tf.clip_by_value(x_input + eps, -1.0, 1.0)
+        x_min = tf.clip_by_value(x_input - eps, -1.0, 1.0)
 
-    target_class_input = tf.placeholder(tf.int32, shape=[FLAGS.batch_size])
-    one_hot_target_class = tf.one_hot(target_class_input, num_classes)
+        with slim.arg_scope(inception.inception_v3_arg_scope()):
+            inception.inception_v3(
+                x_input, num_classes=num_classes, is_training=False)
 
-    cross_entropy = 0
+        target_class_input = tf.placeholder(tf.int32, shape=[FLAGS.batch_size])
+        one_hot_target_class = tf.one_hot(target_class_input, num_classes)
 
-    with slim.arg_scope(inception.inception_v3_arg_scope()):
-      logits, end_points = inception.inception_v3(
-        x_input, num_classes=num_classes, is_training=False, reuse=True)
-    cross_entropy += tf.losses.softmax_cross_entropy(one_hot_target_class,
-                                                     logits)
+        cross_entropy = 0
 
-    scaled_signed_grad = eps * tf.sign(tf.gradients(cross_entropy, x_input)[0])
-    x_adv = tf.stop_gradient(x_input + scaled_signed_grad)
-    x_adv = tf.clip_by_value(x_adv, x_min, x_max)
+        with slim.arg_scope(inception.inception_v3_arg_scope()):
+            logits, end_points = inception.inception_v3(
+                x_input, num_classes=num_classes, is_training=False, reuse=True)
+        cross_entropy += tf.losses.softmax_cross_entropy(one_hot_target_class,
+                                                         logits)
 
-    # Run computation
-    with tf.Session() as sess:
-      saver = tf.train.Saver(
-        tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='InceptionV3'))
-      saver.restore(sess, 'inception_v3.ckpt')
+        scaled_signed_grad = eps * \
+            tf.sign(tf.gradients(cross_entropy, x_input)[0])
+        x_adv = tf.stop_gradient(x_input + scaled_signed_grad)
+        x_adv = tf.clip_by_value(x_adv, x_min, x_max)
 
-      for filenames, images in load_images(FLAGS.input_dir, batch_shape):
-        adv_images = sess.run(x_adv, feed_dict={x_input: images})
-        save_images(adv_images, filenames, FLAGS.output_dir)
+        # Run computation
+        with tf.Session() as sess:
+            saver = tf.train.Saver(
+                tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope='InceptionV3'))
+            saver.restore(sess, 'inception_v3.ckpt')
+
+            for filenames, images in load_images(FLAGS.input_dir, batch_shape):
+                adv_images = sess.run(x_adv, feed_dict={x_input: images})
+                save_images(adv_images, filenames, FLAGS.output_dir)
 
 
 if __name__ == '__main__':
-  tf.app.run()
+    tf.app.run()
