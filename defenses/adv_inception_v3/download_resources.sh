564750617ec63a21dfc10a5f6c369e5f4ceb96de
Scott Lowe <scott.code.lowe@gmail.com>
2017-09-04 22:15:12 +0100

    RF: Use our own download shell script method for sample submissions

---
+++ defenses/adv_inception_v3/download_resources.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Script to download checkpoints and other resources for the model.
+#
+
+# directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}"
+
+if [ ! -e "adv_inception_v3.ckpt.data-00000-of-00001" ]; then
+    # Download ensemble adversarially trained inception resnet v2 checkpoint
+    wget http://download.tensorflow.org/models/adv_inception_v3_2017_08_18.tar.gz
+    tar -xvzf adv_inception_v3_2017_08_18.tar.gz
+    rm adv_inception_v3_2017_08_18.tar.gz
+fi
--- download_data.sh
+++ download_data.sh
@@ -2,11 +2,6 @@
 
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 
-# Download checkpoints for sample attacks and defenses.
-attacks/download_checkpoints.sh
-targeted_attacks/download_checkpoints.sh
-defenses/download_checkpoints.sh
-
 # Download dataset.
 mkdir dataset/images
 python dataset/download_images.py \
