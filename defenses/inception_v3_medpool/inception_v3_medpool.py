f2cf16e010cf4f4ff2bdf91c7976e8770220f5ac
Scott Lowe <scott.code.lowe@gmail.com>
2017-09-20 03:19:14 +0100

    BUG: Fix padding in first conv

--- defenses/inception_v3_medpool/inception_v3_medpool.py
+++ defenses/inception_v3_medpool/inception_v3_medpool.py
@@ -105,7 +105,7 @@ def inception_v3_base(inputs,
                         padding='VALID'):
       # 299 x 299 x 3
       end_point = 'Conv2d_1a_3x3'
-      net = slim.conv2d(inputs, depth(32), [3, 3], stride=1, scope=end_point)
+      net = slim.conv2d(inputs, depth(32), [3, 3], stride=1, padding='SAME', scope=end_point)
       net = tf_median_pool(net, [3, 3], strides=[2, 2])
       end_points[end_point] = net
       if end_point == final_endpoint: return net, end_points
