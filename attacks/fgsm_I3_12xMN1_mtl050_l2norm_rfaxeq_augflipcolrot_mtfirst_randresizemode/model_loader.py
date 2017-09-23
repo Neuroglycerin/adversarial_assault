BUG: Can't take gradient of bicubic, so avoid it

attacks/fgsm_I3_12xMN1_mtl050_l2norm_rfaxeq_augflipcolrot_mtfirst_randresizemode/model_loader.py
@@ -148,7 +148,7 @@ class ModelLoader():
         num_cases = 3
         case_map = lambda case: 3 if case is 2 else case
         x = control_flow_ops.merge([
-            func(control_flow_ops.switch(x, tf.equal(resize_mode, case_map(case)))[1], case)
+            func(control_flow_ops.switch(x, tf.equal(resize_mode, case))[1], case_map(case))
             for case in range(num_cases)])[0]
 
         kwargs = {}