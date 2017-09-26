eaab63cbffc88d17e09eda2ff149b0e2bbc60fe7
Scott Lowe <scott.code.lowe@gmail.com>
2017-09-26 19:53:27 +0100

    Swap fgsma checkpoints to fresh downloads

--- attacks/fgsma_I3_12MN/download_resources.sh
+++ attacks/fgsma_I3_12MN/download_resources.sh
@@ -13,22 +13,94 @@ if [ -d "slim" ]; then
 fi
 cp -r ../../tensorflow-slim ./slim
 
-MODELS_SOURCE_DIR=../../models
-MODELS_OUT_DIR="${SCRIPT_DIR}/models"
 
-mkdir -p "${MODELS_OUT_DIR}"
+if [ ! -e "inception_v3.ckpt" ]; then
+    # Control will enter here if file doesn't exist.
+    wget http://download.tensorflow.org/models/inception_v3_2016_08_28.tar.gz
+    tar -xvzf inception_v3_2016_08_28.tar.gz
+    rm inception_v3_2016_08_28.tar.gz
+fi
 
-cp -r "${MODELS_SOURCE_DIR}"/inception_v3 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_100_224 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_100_192 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_100_160 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_100_128 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_075_224 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_075_192 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_075_160 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_075_128 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_050_224 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_050_192 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_050_160 "${MODELS_OUT_DIR}"
-cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_050_128 "${MODELS_OUT_DIR}"
+
+# 70.7
+if [ ! -e "mobilenet_v1_1.0_224.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_1.0_224_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_1.0_224_2017_06_14.tar.gz
+    rm mobilenet_v1_1.0_224_2017_06_14.tar.gz
+fi
+
+# 69.3
+if [ ! -e "mobilenet_v1_1.0_192.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_1.0_192_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_1.0_192_2017_06_14.tar.gz
+    rm mobilenet_v1_1.0_192_2017_06_14.tar.gz
+fi
+
+# 67.2
+if [ ! -e "mobilenet_v1_1.0_160.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_1.0_160_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_1.0_160_2017_06_14.tar.gz
+    rm mobilenet_v1_1.0_160_2017_06_14.tar.gz
+fi
+
+# 64.1
+if [ ! -e "mobilenet_v1_1.0_128.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_1.0_128_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_1.0_128_2017_06_14.tar.gz
+    rm mobilenet_v1_1.0_128_2017_06_14.tar.gz
+fi
+
+
+# 68.4
+if [ ! -e "mobilenet_v1_0.75_224.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.75_224_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.75_224_2017_06_14.tar.gz
+    rm mobilenet_v1_0.75_224_2017_06_14.tar.gz
+fi
+
+# 67.4
+if [ ! -e "mobilenet_v1_0.75_192.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.75_192_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.75_192_2017_06_14.tar.gz
+    rm mobilenet_v1_0.75_192_2017_06_14.tar.gz
+fi
+
+# 65.2
+if [ ! -e "mobilenet_v1_0.75_160.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.75_160_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.75_160_2017_06_14.tar.gz
+    rm mobilenet_v1_0.75_160_2017_06_14.tar.gz
+fi
+
+# 61.8
+if [ ! -e "mobilenet_v1_0.75_128.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.75_128_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.75_128_2017_06_14.tar.gz
+    rm mobilenet_v1_0.75_128_2017_06_14.tar.gz
+fi
+
+
+if [ ! -e "mobilenet_v1_0.50_224.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.50_224_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.50_224_2017_06_14.tar.gz
+    rm mobilenet_v1_0.50_224_2017_06_14.tar.gz
+fi
+
+if [ ! -e "mobilenet_v1_0.50_192.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.50_192_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.50_192_2017_06_14.tar.gz
+    rm mobilenet_v1_0.50_192_2017_06_14.tar.gz
+fi
+
+if [ ! -e "mobilenet_v1_0.50_160.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.50_160_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.50_160_2017_06_14.tar.gz
+    rm mobilenet_v1_0.50_160_2017_06_14.tar.gz
+fi
+
+if [ ! -e "mobilenet_v1_0.50_128.ckpt.data-00000-of-00001" ]; then
+    wget http://download.tensorflow.org/models/mobilenet_v1_0.50_128_2017_06_14.tar.gz
+    tar -xvzf mobilenet_v1_0.50_128_2017_06_14.tar.gz
+    rm mobilenet_v1_0.50_128_2017_06_14.tar.gz
+fi
 
