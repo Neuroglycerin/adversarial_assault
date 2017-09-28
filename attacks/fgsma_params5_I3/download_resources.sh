2e00dbf037055d84f642db4ce8ea6a6984ba5f46
Scott Lowe <scott.code.lowe@gmail.com>
2017-09-28 02:11:13 +0100

    Try adding more models

---
+++ attacks/fgsma_params5_I3/download_resources.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#
+# Script to download checkpoints and other resources for the model.
+#
+
+# directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}"
+
+if [ -d "slim" ]; then
+    rm -rf slim
+fi
+cp -r ../../tensorflow-slim ./slim
+
+MODELS_SOURCE_DIR=../../models
+MODELS_OUT_DIR="${SCRIPT_DIR}/models"
+
+mkdir -p "${MODELS_OUT_DIR}"
+
+cp -r "${MODELS_SOURCE_DIR}"/inception_v3 "${MODELS_OUT_DIR}"
