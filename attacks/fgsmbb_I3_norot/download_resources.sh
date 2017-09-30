0ff3aced453016a68ee42269a6cef84f0c16d78a
Scott Lowe <scott.code.lowe@gmail.com>
2017-09-30 02:34:52 +0100

    Remove rotation aug

---
+++ attacks/fgsmbb_I3_norot/download_resources.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Script to download checkpoints and other resources for the model.
+#
+
+# directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}"
+
+if [ -d "slim" ]; then
+    rm -rf slim
+fi
+cp -r ../../tensorflow-slim ./slim
+
+MODELS_SOURCE_DIR=../../models
+MODELS_OUT_DIR="${SCRIPT_DIR}/models"
+
+mkdir -p "${MODELS_OUT_DIR}"
+
+cp -r "${MODELS_SOURCE_DIR}"/inception_* "${MODELS_OUT_DIR}"
+cp -r "${MODELS_SOURCE_DIR}"/mobilenet_* "${MODELS_OUT_DIR}"
