902f7e58e82c0fb398e22ce8f00a78aecd49ab81
Scott Lowe <scott.code.lowe@gmail.com>
2017-09-30 23:40:38 +0100

    d round of trials

---
+++ attacks/fgsmd_I3a5_MN100-224_I1_6aug_MNlah200-299/download_resources.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Script to download checkpoints and other resources for the model.
+#
+
+# directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}"
+
+if [ -d "slim" ]; then
+    rm -rf slim
+fi
+cp -r ../../tensorflow-slim ./slim
+
+MODELS_SOURCE_DIR=../../models
+CUSTOM_MODELS_SOURCE_DIR=../../custom_models
+MODELS_OUT_DIR="${SCRIPT_DIR}/models"
+
+mkdir -p "${MODELS_OUT_DIR}"
+
+cp -r "${CUSTOM_MODELS_SOURCE_DIR}"/inception_v3_5aux_299 "${MODELS_OUT_DIR}"
+cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_100_224 "${MODELS_OUT_DIR}"
+cp -r "${MODELS_SOURCE_DIR}"/inception_v1 "${MODELS_OUT_DIR}"
+cp -r "${CUSTOM_MODELS_SOURCE_DIR}"/mobilenet_v1_lahrelu_200_flab_299 "${MODELS_OUT_DIR}"
