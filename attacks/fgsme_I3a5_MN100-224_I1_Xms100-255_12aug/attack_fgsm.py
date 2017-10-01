f2c87d3ca501cc5beb247d333dd321972463cf0c
Scott Lowe <scott.code.lowe@gmail.com>
2017-10-01 01:19:59 +0100

    Actually use 12 aug

--- attacks/fgsme_I3a5_MN100-224_I1_Xms100-255_12aug/attack_fgsm.py
+++ attacks/fgsme_I3a5_MN100-224_I1_Xms100-255_12aug/attack_fgsm.py
@@ -52,7 +52,7 @@ tf.flags.DEFINE_float(
     'p_threshold_antitarget', 0.50, 'What split of probabilities to anti-target.')
 
 tf.flags.DEFINE_integer(
-    'num_aug', 8, 'Number of augmented repetitions of the image to each net.')
+    'num_aug', 12, 'Number of augmented repetitions of the image to each net.')
 
 tf.flags.DEFINE_integer(
     'max_iter', 1, 'Maximum number of iterations.')
