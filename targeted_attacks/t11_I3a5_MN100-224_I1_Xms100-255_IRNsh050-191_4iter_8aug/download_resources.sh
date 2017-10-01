26eee09e03495a59c5e97a0dce7fa38d82618a9d
Scott Lowe <scott.code.lowe@gmail.com>
2017-10-01 23:16:18 +0100

    Try it?

---
+++ targeted_attacks/t11_I3a5_MN100-224_I1_Xms100-255_IRNsh050-191_4iter_8aug/download_resources.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#
+# Script to download checkpoints and other resources for the model.
+#
+
+# directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}"
+
+if [ -d "slim" ]; then
+    rm -rf slim
+fi
+cp -r ../../tensorflow-slim ./slim
+
+MODELS_SOURCE_DIR=../../models
+CUSTOM_MODELS_SOURCE_DIR=../../custom_models
+MODELS_OUT_DIR="${SCRIPT_DIR}/models"
+
+mkdir -p "${MODELS_OUT_DIR}"
+
+cp -r "${CUSTOM_MODELS_SOURCE_DIR}"/inception_v3_5aux_299 "${MODELS_OUT_DIR}"
+cp -r "${MODELS_SOURCE_DIR}"/mobilenet_v1_100_224 "${MODELS_OUT_DIR}"
+cp -r "${CUSTOM_MODELS_SOURCE_DIR}"/xception_multiscale2_flab_255 "${MODELS_OUT_DIR}"
+cp -r "${CUSTOM_MODELS_SOURCE_DIR}"/inception_resnet_v2_shallow_050_191 "${MODELS_OUT_DIR}"
