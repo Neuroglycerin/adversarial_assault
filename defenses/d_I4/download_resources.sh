f1a054590e45413b616008e576a8cb54edb8dcc7
Scott Lowe <scott.code.lowe@gmail.com>
2017-10-01 05:04:57 +0100

    Try out lahrelu defences

---
+++ defenses/d_I4/download_resources.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+# Script to download checkpoints and other resources for the model.
+#
+
+# directory where this script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd "${SCRIPT_DIR}"
+
+if [ -d "slim" ]; then
+    rm -rf slim
+fi
+cp -r ../../tensorflow-slim ./slim
+
+MODELS_SOURCE_DIR=../../models
+CUSTOM_MODELS_SOURCE_DIR=../../custom_models
+MODELS_OUT_DIR="${SCRIPT_DIR}/models"
+
+mkdir -p "${MODELS_OUT_DIR}"
+
+cp -r "${MODELS_SOURCE_DIR}"/inception_v4 "${MODELS_OUT_DIR}"
